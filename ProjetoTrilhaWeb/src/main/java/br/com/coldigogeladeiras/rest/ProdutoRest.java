6d678f4836abf53469b4b679923e4c62607762a2
Ro4Ever <robertoellmer@gmail.com>
2024-04-25 21:05:49 -0300

    REST

--- ProjetoTrilhaWeb/src/main/java/br/com/coldigogeladeiras/rest/ProdutoRest.java
+++ ProjetoTrilhaWeb/src/main/java/br/com/coldigogeladeiras/rest/ProdutoRest.java
@@ -11,6 +11,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.PathParam;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
@@ -72,6 +74,33 @@ public class ProdutoRest extends UtilRest{
 			
 			String json = new Gson().toJson(listaProdutos);
 			return this.buildResponse(json);
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			return this.buildErrorResponse(e.getMessage());
+		}
+		
+	}
+	@DELETE
+	@Path("/excluir/{id}")
+	@Consumes("application/*")
+	public Response excluir(@PathParam("id") int id) {
+		
+		try {
+			Conexao conec = new Conexao();
+			Connection conexao = conec.abrirConexao();
+			JDBCProdutoDAO jdbcProduto = new JDBCProdutoDAO(conexao);
+			
+			boolean retorno = jdbcProduto.deletar(id);
+			
+			String msg = "";
+			if(retorno) {
+				msg = "Produto excluído com sucesso!";
+			}else {
+				msg = "Erro ao excluir produto.";
+			}
+			
+			return this.buildResponse(msg);
 			
 		}catch(Exception e) {
 			e.printStackTrace();
